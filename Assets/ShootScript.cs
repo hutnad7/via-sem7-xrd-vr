db0ceddd171e3d86a5a1d2f803c50725c5970717
Huthifa <hozyfa954@gmail.com>
2023-11-16 11:30:51 +0100

    Update shoot script and the scene
    Co-Authored-By: Stefan Georgiev <steffan.g23@gmail.com>

--- Assets/ShootScript.cs
+++ Assets/ShootScript.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ShootScript : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletCube;
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform spawnpoint;
+    [SerializeField] private float bulletSpeed = 10f;
 
     public void Shoot()
     {
         bullet.SetActive(true);
-        Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
+       GameObject spawnBullet = Instantiate(bulletCube, spawnpoint.position, spawnpoint.rotation);
+       spawnBullet.GetComponent<Rigidbody>().velocity = spawnpoint.forward * bulletSpeed;
+       Destroy(spawnBullet, 1.5f);
     }
 }
